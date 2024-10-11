71157bfc55f45bf2bbfd9f6adba62eef64475a66
isabeletorres <165826756+isabeletorres@users.noreply.github.com>
2024-10-11 16:11:57 -0300

    Add project files.

---
+++ course/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args){
+            
+            string[] vet =Console.ReadLine().Split(' ');
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3]);
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(altura);
+            Console.WriteLine(idade);
+        }
+    }
+}
+
