e683a02032dc69ef6e85235574b4fb796064776a
isabeletorres <165826756+isabeletorres@users.noreply.github.com>
2024-10-11 17:08:00 -0300

    OOP-Csharp

--- course/Program.cs
+++ course/Program.cs
@@ -1,18 +1,36 @@
 ﻿using System;
-
+using System.Globalization;
 namespace Course {
     class Program {
         static void Main(string[] args){
+            double xa, xb, xc, ya, yb, yc;
             
-            string[] vet =Console.ReadLine().Split(' ');
-            string nome = vet[0];
-            char sexo = char.Parse(vet[1]);
-            int idade = int.Parse(vet[2]);
-            double altura = double.Parse(vet[3]);
-            Console.WriteLine(nome);
-            Console.WriteLine(sexo);
-            Console.WriteLine(altura);
-            Console.WriteLine(idade);
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            xa = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xb = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xc = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            ya = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yb = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yc = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double px = (xa + xb + xc) / 2;
+            double py = (ya +yb + yc) / 2;
+
+            double areaX = Math.Sqrt(px * (px - xa) * (px - xb) * (px - xc));
+            double areaY = Math.Sqrt(py * (py - ya) * (py - yb) * (py - yc));
+
+            Console.WriteLine($"Área de x= {areaX:F4}");
+            Console.WriteLine($"Área de x= {areaY:F4}");
+
+            if (areaX > areaY) {
+                Console.WriteLine("Mior área: X");
+            } else if (areaY> areaX) {
+                Console.WriteLine("Mior área: y");
+            } else {
+                Console.WriteLine("As áreas são iguais");
+            }
         }
     }
 }
