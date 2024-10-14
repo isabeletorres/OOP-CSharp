7762434714f5aa886eadbb236970ce342b062b85
isabeletorres <165826756+isabeletorres@users.noreply.github.com>
2024-10-14 09:11:17 -0300

    OOP-Csharp

--- course/Program.cs
+++ course/Program.cs
@@ -36,6 +36,30 @@ namespace Course {
             } else {
                 Console.WriteLine("As áreas são iguais");
             }
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+
+            Console.WriteLine("Dados da primeira pessoa: ");
+            Console.WriteLine("Nome: ");
+            p1.Nome = Console.ReadLine();
+            Console.WriteLine("Idade: ");
+            p1.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa: ");
+            Console.WriteLine("NOme: ");
+            p2.Nome = Console.ReadLine();
+            Console.WriteLine("Idade: ");
+            p2.Idade = int.Parse(Console.ReadLine());
+
+            if (p1.Idade > p2.Idade) {
+                Console.WriteLine($"Pessoa mais velha: {p1.Nome}");
+            }
+            else if (p2.Idade > p1.Idade) {
+                Console.WriteLine($"Pessoa mais velha: {p2.Nome}");
+            }
+            else {
+                Console.WriteLine($"{p1.Nome} e {p2.Nome} tem a mesma idade");
+            }
         }
     }
 }
