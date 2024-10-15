fa608e3209b04e785f94e6378ffc5d3e8d3f827c
isabeletorres <165826756+isabeletorres@users.noreply.github.com>
2024-10-15 11:33:32 -0300

    Sobrecarga

---
+++ Sobrecarga/Program.cs
@@ -0,0 +1,35 @@
+﻿using Sobrecarga;
+using System;
+using System.Globalization;
+namespace Sobrecarga {
+    class Program {
+        static void Main(String[] args) {
+
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome do produto: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+          
+            Produt p = new Produt(nome, preco);
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser adicionados ao estoque: ");
+            int qtde = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qtde);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            int qtdremove = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qtdremove);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+
+        }
+    }
+}
