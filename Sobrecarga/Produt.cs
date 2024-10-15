b8d89741095e526933e50c6dc36a0b9da166b908
isabeletorres <165826756+isabeletorres@users.noreply.github.com>
2024-10-15 16:51:40 -0300

    Private

--- Sobrecarga/Produt.cs
+++ Sobrecarga/Produt.cs
@@ -4,38 +4,52 @@ using System.Net;
 
 namespace Sobrecarga {
     internal class Produt {
-        public string Nome;
-        public double Preco;
-        public int Quantidade;
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
 
         public Produt(){
-            Quantidade = 10;
-        }
-        public Produt(string nome, double preco) : this() {
-            Nome = nome;
-            Preco = preco;
+            
         }
 
-        public Produt(string nome, double preco, int quantidade) : this(nome, preco) {
-            Quantidade = quantidade;
+        public Produt(string nome, double preco, int quantidade) {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+        public string GetNome() {
+            return _nome;
+        }
+        public double GetPreco() {
+            return _preco;
+        }
+        public int GetQuantidade() {
+            return _quantidade;
+        }
+        public void SetNome(string nome) {
+            if (nome != null && nome.Length > 1) {
+                _nome = nome;
+            }
+           
         }
 
         public double ValorTotalEmEstoque() {
-            return Preco * Quantidade;
+            return _preco * _quantidade;
         }
+
         public void AdicionarProdutos(int quantidade) {
-            Quantidade += quantidade;
+            _quantidade += quantidade;
         }
 
         public void RemoverProdutos(int quantidade) {
-            Quantidade -= quantidade;
+            _quantidade -= quantidade;
         }
         public override string ToString() {
-            return Nome
+            return _nome
                 + ", $"
-                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + _preco.ToString("F2", CultureInfo.InvariantCulture)
                 + ", "
-                + Quantidade
+                + _quantidade
                 + " unidades, Total $ "
                 + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
--- Sobrecarga/Program.cs
+++ Sobrecarga/Program.cs
@@ -5,38 +5,11 @@ namespace Sobrecarga {
     class Program {
         static void Main(String[] args) {
 
-            Console.WriteLine("Entre com os dados do produto: ");
-            Console.Write("Nome do produto: ");
-            string nome = Console.ReadLine();
-            Console.Write("Preço: ");
-            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Produt p = new Produt("TV", 500.00, 10);
 
-            Produt p = new Produt(nome, preco);
-
-            Produt p2 = new Produt {
-                Nome = "TV",
-                Preco = 500,
-                Quantidade = 20
-            };
-
-            Console.WriteLine();
-            Console.WriteLine("Dados do produto: " + p);
-            Console.WriteLine();
-
-            Console.Write("Digite o número de produtos a ser adicionados ao estoque: ");
-            int qtde = int.Parse(Console.ReadLine());
-            p.AdicionarProdutos(qtde);
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
-            Console.WriteLine();
-
-            Console.Write("Digite o número de produtos a ser removido do estoque: ");
-            int qtdremove = int.Parse(Console.ReadLine());
-            p.RemoverProdutos(qtdremove);
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
-            Console.WriteLine();
-
+            p.SetNome("T");
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco());
         }
     }
 }
