0bb534f8e80021a07ea47038ff205d6ab56a4d3c
isabeletorres <165826756+isabeletorres@users.noreply.github.com>
2024-10-15 11:19:50 -0300

    Construtores

---
+++ Construtores/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+namespace Construtores {
+    class Program {
+        static void Main(String[] args) {
+           
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome do produto: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto(nome, preco, quantidade);
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser adicionados ao estoque: ");
+            int qtde = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qtde);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            int qtdremove = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qtdremove);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+            
+        }
+    }
+}
