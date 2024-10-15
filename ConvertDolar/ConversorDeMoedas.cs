a845233246e8036529474301795ee3049f89809e
isabeletorres <165826756+isabeletorres@users.noreply.github.com>
2024-10-15 10:02:06 -0300

    ConversaoDolarReal

---
+++ ConvertDolar/ConversorDeMoedas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertDolar {
+    internal class ConversorDeMoedas {
+        public static double IOF = 0.06;
+
+        public static double DolarParaReal(double cotacao, double quantia) {
+            double total = cotacao * quantia;
+            return total + (IOF * total);
+        }
+    }
+}
---
+++ ConvertDolar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Net.WebSockets;
+
+namespace ConvertDolar {
+    class Program {
+        public static void Main(string[] args) {
+            Console.Write("Qual é a cotação do dolar? ");
+            double dolar = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dolares você vai comprar? ");
+            double qtdDolar = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = ConversorDeMoedas.DolarParaReal(qtdDolar, dolar);
+
+            Console.WriteLine("Valor a ser pago em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
