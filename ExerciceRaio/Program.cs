d4bf2872bbdc18b3390b57baf6d4ad16aecb8191
isabeletorres <165826756+isabeletorres@users.noreply.github.com>
2024-10-15 09:41:11 -0300

    cALCULADORA

--- ExerciceRaio/Program.cs
+++ ExerciceRaio/Program.cs
@@ -4,16 +4,16 @@ namespace ExerciceRaio {
     class Program {
         
         static void Main(string[] args) {
-            Calculadora calculadora = new Calculadora();
+           
             Console.Write("Entre o valor do Raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double cir = calculadora.Circunferencia(raio);
-            double volume = calculadora.Volume(raio);
+            double cir = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
 
             Console.WriteLine("Circunferência: "+cir.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de PI: " + calculadora.pi.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculadora.pi.ToString("F2", CultureInfo.InvariantCulture));
 
         }
        
