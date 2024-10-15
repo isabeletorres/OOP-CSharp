dbde2ebe13d7b37ec9fc215bd69b2019de09618e
isabeletorres <165826756+isabeletorres@users.noreply.github.com>
2024-10-15 17:10:17 -0300

    Properties

---
+++ Properties/Program.cs
@@ -0,0 +1,15 @@
+﻿using Sobrecarga;
+using System;
+using System.Globalization;
+namespace Properties {
+    class Program {
+        static void Main(String[] args) {
+
+            Prod p = new Prod("TV", 500.00, 10);
+
+            p.Nome = "TV 4K";
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+        }
+    }
+}
