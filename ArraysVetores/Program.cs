643f5c515eb17e21e0163046de52d3bb7514eab8
isabeletorres <165826756+isabeletorres@users.noreply.github.com>
2024-10-17 13:29:58 -0300

    Arrays exemples

---
+++ ArraysVetores/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace ArraysVetores {
+    class Program {
+        static void Main(string[] args) {
+            double soma = 0;
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n];
+
+            for (int i = 0; i <vect.Length; i++) {
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                soma += vect[i];
+            }
+            double avg = soma/vect.Length;
+            Console.WriteLine("AVERAGE HEIGHT= "+ avg.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
