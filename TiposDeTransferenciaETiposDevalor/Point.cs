c5cc4cf3a23c348acdf8a3e15aa5f30558e84502
isabeletorres <165826756+isabeletorres@users.noreply.github.com>
2024-10-17 11:21:04 -0300

    Tipos referencia e valor e struckt

---
+++ TiposDeTransferenciaETiposDevalor/Point.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiposReferenciaEValor {
+    internal struct Point {
+        public double X; 
+        public double Y;
+
+        public override string ToString() {
+            return "(" +X + ", " +Y + ")";
+        }
+    }
+}
---
+++ TiposDeTransferenciaETiposDevalor/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TiposReferenciaEValor {
+    class Program {
+        static void Main(string[] args) {
+            Point p = new Point();
+
+            Console.WriteLine(p);
+        }
+    }
+}
