f3bb2574055fa51c302821e8b90f14a78dd305df
isabeletorres <165826756+isabeletorres@users.noreply.github.com>
2024-10-18 17:18:35 -0300

    Params Ref and Out

---
+++ RefOut/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefOut {
+    internal class Calculator {
+        public static void Triple0(ref int x) {
+            x *= 3;
+        }
+        public static void Triple1(int origin, out int result ) {
+            result = origin * 3;
+        }
+    }
+
+}
---
+++ RefOut/Program.cs
@@ -0,0 +1,20 @@
+﻿using RefOut;
+using System;
+using System.Globalization;
+
+namespace ExerciceVetores3 {
+    class Program {
+        static void Main(string[] args) {
+            int a = 10;
+            int triple;
+            Console.WriteLine("Using Out");
+            Calculator.Triple1(a, out triple);
+            Console.WriteLine(triple);
+
+            Calculator.Triple0(ref a);
+            Console.WriteLine("Using Ref");
+            Console.WriteLine(a);
+            
+        }
+    }
+}
