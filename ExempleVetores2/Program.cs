9ea83e9ae8d514f40f9bb70f529074f48935c0e1
isabeletorres <165826756+isabeletorres@users.noreply.github.com>
2024-10-18 09:13:24 -0300

    Exercice two (Arrays)

---
+++ ExempleVetores2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExempleVetores2 {
+    class Program {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+           
+            Product[] vect = new Product[n];
+
+            for (int i = 0; i<n; i++) {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vect[i] = new Product { Name = name, Price = price };
+            }
+            double soma = 0.0;
+            for (int i = 0; i < n; i++) {
+                soma += vect[i].Price;
+            }
+            double avg = soma / n;
+
+            Console.WriteLine("AVERAGE PRICE: "+ avg.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
