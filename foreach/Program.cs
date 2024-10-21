34955f572f685bc7e66e3ea8fa4f6311f8e5a04c
isabeletorres <165826756+isabeletorres@users.noreply.github.com>
2024-10-21 16:43:44 -0300

    Foreach

---
+++ foreach/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Globalization;
+
+namespace Foreach {
+    class Program {
+        static void Main(string[] args) {
+            string[] vect = new string[] { "Maria", "Alex", "Bob" };
+
+            foreach (string str in vect) {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
