5c62ad05372381fa9327de60c9140fd18f29cff1
isabeletorres <165826756+isabeletorres@users.noreply.github.com>
2024-10-21 17:50:22 -0300

    Adicionando valores a lista

---
+++ Lista1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Lista1 {
+    class Program {
+        static void Main(string[] args) {
+            List<string> list = new List<string>();
+
+            list.Add("Maria");
+            list.Add("Alex");
+            list.Add("Bob");
+            list.Add("Ana");
+
+            foreach(string obj in list) {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
